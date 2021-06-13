e2e7a9d0e112e3b8045b761dcf2ef3f70499eba4
Jannis <jannis@pixel-pink.de>
2021-06-13 18:07:23 +0200

    fixed imprint

--- src/routes/imprint.svelte
+++ src/routes/imprint.svelte
@@ -2,14 +2,24 @@
 	<title>Jannis Paul Wicke</title>
 </svelte:head>
 
-<h1>Imprint</h1>
-<p>
-	Angaben gemäß § 5 TMG:<br />
-	Jannis Wicke<br />
-	Tempelhofer Damm 206<br />
-	12099 Berlin<br />
-	<br /><br />
-	E-Mail: hi@paul-wicke.de
-	<br /><br />
-	Keine Tracking, keine Cookies
-</p>
+<section>
+	<div class="imprint-container">
+		<h1>Impressum</h1>
+		<p>
+			Angaben gemäß § 5 TMG:<br />
+			Jannis Wicke<br />
+			Tempelhofer Damm 206<br />
+			12099 Berlin<br />
+			<br /><br />
+			E-Mail: hi@paul-wicke.de
+			<br /><br />
+			Keine Tracking, keine Cookies
+		</p>
+	</div>
+</section>
+
+<style>
+section{
+	min-height: calc(100vh - 14rem);
+	
+	}	</style>
